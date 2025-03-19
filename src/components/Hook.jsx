45e131bec7af7d50ad9ba445c19384d0f01ae9c2
pharmacy <thuyhienle1664@gmail.com>
2025-03-19 11:12:35 +0700

    Bai1 + minh chung

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import { useRef } from "react";
 import './Hook.css'
 const reduce = (state, action) => {
     switch (action.type) {
@@ -7,20 +8,36 @@ const reduce = (state, action) => {
         default: return state
     }
 }
-const inputRef = 
 
 function Hook() {
     const [state, dispatch] = useReducer(reduce, { count: 0 })
-
+    const inputRef = useRef(null);
+    function handFocus() {
+        if (inputRef.current) {
+            inputRef.current.focus();
+            console.log(inputRef.current.value);
+        }
+    }
     return (
-        < div className="counter">
-            <h3>Giá trị: {state.count} </h3>
-            <div className="btn-group">
-                <button onClick={() => dispatch({ type: 'increment' })}>Tăng</button>
-                <button onClick={() => dispatch({ type: 'decrement' })}>Giảm</button>
-            </div>
+        <>
+            < div className="counter">
+                <h3>Giá trị: {state.count} </h3>
+                <div className="btn-group">
+                    <button onClick={() => dispatch({ type: 'increment' })}>Tăng</button>
+                    <button onClick={() => dispatch({ type: 'decrement' })}>Giảm</button>
+                </div>
 
-        </div >
+            </div >
+            <div className="useRef">
+                <h3>Connect input text</h3>
+                <input className="input-text" ref={inputRef} type="text" />
+                <div className="btn">
+                    <button className="btn-click" onClick={handFocus}>Click</button>
+                </div>
+
+            </div>
+        </>
+
     );
 }
 
